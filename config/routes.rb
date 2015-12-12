c6fd694783931af473287f720b1fb7db5951a0af
montekaka <josh.chen.jia@gmail.com>
2015-12-12 14:44:11 -0800

    set root to feeds

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   resources :feeds do 
     resources :items
   end
+
+  root 'feeds#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
